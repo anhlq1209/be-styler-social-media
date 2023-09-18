bc243dce4cffafc98269630153679c400da4db93
anhlq1209 <anhlq1209@gmail.com>
2023-09-18 20:06:57 +0700

    installing packages, setting up server

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+mongoose
+  .connect(process.env.MONGOOSE_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Listening at ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.error(error));
