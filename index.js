99e1c9873e96820766b82ff141e8b6c2218210c8
anhlq1209 <anhlq1209@gmail.com>
2023-09-18 20:18:15 +0700

    authentication route

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import "dotenv/config";
+import AuthRoute from "./Routes/AuthRoute.js";
+
+// Routes
 
 const app = express();
 
+// Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
@@ -19,3 +23,6 @@ mongoose
     )
   )
   .catch((error) => console.error(error));
+
+// usage of routes
+app.use('/auth', AuthRoute)
