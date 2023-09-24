940d46c9a21ad086fb069c43f9009fae71af9589
anhlq1209 <anhlq1209@gmail.com>
2023-09-25 02:00:07 +0700

    timeline post aggregation

--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,5 +1,13 @@
 import express from "express";
-import { createPost, deletePost, getPost, getPosts, likePost, updatePost } from "../Controllers/PostController.js";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  getPosts,
+  getTimelinePosts,
+  likePost,
+  updatePost,
+} from "../Controllers/PostController.js";
 
 const router = express.Router();
 
@@ -8,7 +16,7 @@ router.get("/:id", getPost);
 router.put("/:id", updatePost);
 router.delete("/:id", deletePost);
 router.put("/:id/like", likePost);
-
+router.get("/:id/timeline", getTimelinePosts);
 
 router.get("/", getPosts);
 
